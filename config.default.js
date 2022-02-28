d2465926beffb1ec70eee39d4d7f4d833b6df469
rtritto <r.tritto5@studenti.uniba.it>
2022-02-28 23:16:26 +0100

    Fix ME_CONFIG_BASICAUTH_USERNAME env var

--- config.default.js
+++ config.default.js
@@ -138,7 +138,7 @@ module.exports = {
   // set useBasicAuth to true if you want to authenticate mongo-express logins
   // if admin is false, the basicAuthInfo list below will be ignored
   // this will be true unless ME_CONFIG_BASICAUTH_USERNAME is set and is the empty string
-  useBasicAuth: getFileEnv(basicAuthUsername) !== '',
+  useBasicAuth: getFileEnv(basicAuthUsername) !== undefined,
 
   basicAuth: {
     username: getFileEnv(basicAuthUsername) || 'admin',
